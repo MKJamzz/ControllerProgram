2d0260d690e56364ba4987f5a98988e63a73ced5
kevin <trankevin789@gmail.com>
2025-05-22 17:13:18 -0400

    added booleans and helper functions

--- cameraStream.cpp
+++ cameraStream.cpp
@@ -1,5 +1,19 @@
 //add includes needed here
 
+//false on default still have to create helper functions to test if camera/vr are connected; turns true if connected
+bool cameraIsConnected = false;
+bool vrIsConnected = false;
+
+bool checkCameraConnected(){
+// checks if camera is properly connected
+  return true;
+}
+
+bool checkVrConnected(){
+// checks if vr is properly connected
+  return true;
+}
+
 void startCamera(){
 // initalizes the Rasberry Pi Camera
 }
