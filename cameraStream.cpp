bbba3f063b1a5b31ea0f9614d8e164d4a7e25ec9
kevin <trankevin789@gmail.com>
2025-05-22 16:51:49 -0400

    just created starter functions we'd need to setup the camera

--- cameraStream.cpp
+++ cameraStream.cpp
@@ -1,4 +1,18 @@
-int main(){
+//add includes needed here
 
+void startCamera(){
+// initalizes the Rasberry Pi Camera
 }
 
+void startVR(){
+//initalizes the VR SDK
+}
+
+void streamCameraToVR(){
+  while(true){
+  //captures frames from camera
+  //sends frame to vr display
+  //handles fps you want to set
+  }
+
+}
