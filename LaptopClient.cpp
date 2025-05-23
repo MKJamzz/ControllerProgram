c8842b64565f2439c93d0aff165327b1b9ceeb9b
Michael Whiteman <michaelwhiteman123@gmail.com>
2025-05-23 19:06:52 -0400

    New Files for server and client programs

---
+++ LaptopClient.cpp
@@ -0,0 +1,23 @@
+// This program sends the data from the laptop like the controller inputs over to the 
+// server that is being run by the Raspberry PI in order to achieve the 
+// desired GPIO outputs 
+
+#include <SDL.h>
+#include <cpr/cpr.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+
+int main(){
+
+
+
+
+
+
+
+
+    SDL_GameControllerClose(bunga);
+    SDL_Quit();
+    return 0;
+}
