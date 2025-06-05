37dcb7181118f07e335ef0c5986609a3ffc5334b
AdrianShah <152305520+AdrianShah@users.noreply.github.com>
2025-06-05 15:14:09 -0400

    Update CarServer.cpp

--- CarServer.cpp
+++ CarServer.cpp
@@ -6,17 +6,17 @@
 #include <cstdint>
 using namespace std;
 
+//Function decloration
+
 int main(){
     gpioInitialise();
 
-    // PWM PINS
+    //PWM PINS
     const int throtPin = 18;
     const int steerPin = 19;
-    const int servoPin = 13; 
 
     gpioSetPWMfrequency(throtPin, 1000);
     gpioSetPWMfrequency(steerPin, 1000);
-    gpioSetPWMfrequency(servoPin, 50);  // servo usually 50 Hz, good!
 
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in address{AF_INET, htons(8000), INADDR_ANY};
@@ -28,23 +28,23 @@ int main(){
     int client_fd = accept(server_fd, nullptr, nullptr);
     cout << "Connected!?!?" << endl;
 
-    uint8_t buffer[3];
+    uint8_t buffer[4];
 
-    //while(read(client_fd, buffer, 3) == 3){
-    while(true){
+    while(read(client_fd, buffer, 4) == 4){
         gpioPWM(steerPin, buffer[0]);
         gpioPWM(throtPin, buffer[1]);
-        gpioPWM(servoPin, buffer[2]); 
-
-        cout << "Steering PWM: " << static_cast<int>(buffer[0])
-             << " | Throttle PWM: " << static_cast<int>(buffer[1])
-             << " | Servo PWM (GPIO 13): " << static_cast<int>(buffer[2]) << endl;
+        
+          if (buffer[2] == 1 || buffer[3] == 1) {
+        // Exit signal from controller or Ctrl+C
+        std::cout << "\n[INFO] Stop signal received\n";
+        break;
+    }
+  
     }
 
-    // Turning pins off
+    //Turning pins off
     gpioPWM(steerPin, 0);
     gpioPWM(throtPin, 0);
-    gpioPWM(servoPin, 0);
 
     close(client_fd);
     close(server_fd);
