bb23206e13c52f8363dac57fc43d39bf82ab74d8
Michael Whiteman <michaelwhiteman123@gmail.com>
2025-07-23 14:54:30 -0400

    speed fix

--- CarServer.cpp
+++ CarServer.cpp
@@ -60,8 +60,8 @@ int main() {
     
 
         int input = buffer[4];
-        int pulseWidth = remap(input, 0, 255, 1476, 696);
-        int reverseSpeed = remap(buffer[5], 0, 255, 0 , 100);   //moves the car backward
+        int pulseWidth = remap(input, 0, 255, 696, 1476);
+        int reverseSpeed = remap(buffer[5], 0, 255, 0 , 50);   //moves the car backward
         int forwardSpeed = remap(buffer[1], 0, 255, 0 , 100);   //moves the car forward
 
     if (forwardSpeed > 5 && reverseSpeed <= 5) {
