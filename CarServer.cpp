683cc9842914d77d746802dc5c9e7f9c53c4bed6
Michael Whiteman <michaelwhiteman123@gmail.com>
2025-07-23 15:33:56 -0400

    hol up

--- CarServer.cpp
+++ CarServer.cpp
@@ -61,8 +61,8 @@ int main() {
 
         int input = buffer[4];
         int pulseWidth = remap(input, 0, 255, 1476, 696);
-        int reverseSpeed = remap(buffer[5], 0, 255, 0 , 50);   //moves the car backward
-        int forwardSpeed = remap(buffer[1], 0, 255, 0 , 100);   //moves the car forward
+        int reverseSpeed = remap(buffer[1], 0, 255, 0 , 50);   //moves the car backward
+        int forwardSpeed = remap(buffer[5], 0, 255, 0 , 100);   //moves the car forward
 
     if (forwardSpeed > 5 && reverseSpeed <= 5) {
         gpioWrite(R_EN, 1);
